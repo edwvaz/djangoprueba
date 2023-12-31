0ffdeb0dffe2bc93c0c356d388324b6c28b598a7
edwvaz <edwvazmor@gmail..com>
2023-12-31 19:00:34 -0300

    5commit

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -79,15 +79,9 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        dj_database_url.config(
+    'default': {dj_database_url.config(
             default='postgresql://postgres:postgres@localhost/postgres',
-            conn_max_age=600
-        )
-
-    }
+            conn_max_age=600)}
 }
 
 
